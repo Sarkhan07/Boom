7d1d212df56ea38c41b178213d0e52f91614276e
Sarkhan <abdullayevsarxan92@gmail.com>
2026-03-31 23:29:06 +0400

    adding documents

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+const Colors = {
+  light: {
+    surface: "#ffffff",
+    background: "#f5f5f5",
+    border: "#e0e0e0",
+    text: "#000000",
+    textSecondary: "#666666"
+  }
+};
+export default Colors;
